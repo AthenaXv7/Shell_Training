88794ce78146ce45baff1891cf6f06d3f57f3dde
AthenaXv7 <AthenaXv7>
2023-09-27 06:30:27 +0000

    New Files

---
+++ (Clone) ETL Json.py
@@ -0,0 +1,40 @@
+# Databricks notebook source
+from pyspark.sql.functions import *
+
+# COMMAND ----------
+
+# MAGIC %fs ls dbfs:/mnt/
+
+# COMMAND ----------
+
+# MAGIC %fs ls dbfs:/mnt/saunextadls/raw/
+
+# COMMAND ----------
+
+df=spark.read.json("dbfs:/mnt/saunextadls/raw/json/")
+
+# COMMAND ----------
+
+df.display()
+
+# COMMAND ----------
+
+df1 = df.withColumn("ingestiondate", current_timestamp()).withColumn("path", input_file_name())
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC create schema if not exists json
+
+# COMMAND ----------
+
+df1.write.mode("overwrite").option("path","dbfs:/mnt/saunextadls/raw/output/athena/json").saveAsTable("json.bronze")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select count(*) from json.bronze
+
+# COMMAND ----------
+
+
